d9bc650f1b648ab898dd13a7c910414dd1f941f7
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-11-20 21:58:31 +0000

    update key study dates

--- analysis/design/study-dates.R
+++ analysis/design/study-dates.R
@@ -10,14 +10,9 @@ fs::dir_create(here::here("lib", "design"))
 # define key dates ----
 
 study_dates <- tibble::lst(
-  studystart_date = "2021-10-29", # first possible study entry date (when both moderna and pfizer being administered for boosters), and index date for dates as "time since index date" format
-  studyend_date = "2022-02-25", # last study entry dates
-  followupend_date = "2022-07-01", # end of follow-up
-  postestfollowupend_date = "2022-03-31", # end of follow-up for postest, when free community testing stopped
-  firstpfizer_date = "2020-12-08", # first pfizer vaccination in national roll-out
-  firstaz_date = "2021-01-04", # first az vaccination in national roll-out
-  firstmoderna_date = "2021-04-13", # first moderna vaccination in national roll-out
-
+  studystart_date = "2023-04-01", # first possible study entry date (when both moderna and pfizer and sanofi being administered for boosters), and index date for dates as "time since index date" format
+  studyend_date = "2023-06-30", # last study entry dates
+  followupend_date = "2023-08-31", # end of follow-up
   firstpossiblevax_date = "2020-06-01", # used to catch "real" vaccination dates (eg not 1900-01-01)
 )
 
