3b1b159fac43296a1169144d6157a6b09927e51c
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-11-21 20:49:17 +0000

    combine estimates and release

--- analysis/release_objects.R
+++ analysis/release_objects.R
@@ -42,15 +42,6 @@ for(matchset in c("A", "B")){
   fs::file_copy(here("output", "match", matchset, "combined", "followup_rounded.csv"), fs::path(output_dir, "followup_rounded.csv"), overwrite = TRUE)
   fs::file_copy(here("output", "match", matchset, "combined", "followup_treatment_rounded.csv"), fs::path(output_dir, "followup_treatment_rounded.csv"), overwrite = TRUE)
 
-  ## era Contrasts ----
-
-  fs::file_copy(here("output", "match", matchset, "combined", "km_era_estimates_rounded.csv"), fs::path(output_dir, "km_era_estimates_rounded.csv"), overwrite = TRUE)
-  fs::file_copy(here("output", "match", matchset, "combined", "contrasts_era_daily_rounded.csv"), fs::path(output_dir, "contrasts_era_daily_rounded.csv"), overwrite = TRUE)
-  fs::file_copy(here("output", "match", matchset, "combined", "contrasts_era_cuts_rounded.csv"), fs::path(output_dir, "contrasts_era_cuts_rounded.csv"), overwrite = TRUE)
-  fs::file_copy(here("output", "match", matchset, "combined", "contrasts_era_overall_rounded.csv"), fs::path(output_dir, "contrasts_era_overall_rounded.csv"), overwrite = TRUE)
-  fs::file_copy(here("output", "match", matchset, "combined", "contrasts_era_20_rounded.csv"), fs::path(output_dir, "contrasts_era_20_rounded.csv"), overwrite = TRUE)
-
-
 }
 
 ## create text for output review issue ----
