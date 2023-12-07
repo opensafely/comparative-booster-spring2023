803ecd562fa43e1a7456157e1167d54af8a696dd
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-12-07 11:13:13 +0000

    undo bool-factor workaround
    undoes commit https://github.com/opensafely/comparative-booster/commit/072c25fa4dcf28101d780885a28b9c4f4fbb13f6 because bool-factors from case statements are now returned as booleans in ehrql

--- analysis/data_process.R
+++ analysis/data_process.R
@@ -29,10 +29,6 @@ fs::dir_create(here("output", "data"))
 ## Import extracted dataset ----
 data_extract <- read_feather(here("output", "extracts", "extract.arrow"))
 
-boolfactor2bool <- function(x){
-  if_else(x=="TRUE", TRUE, FALSE, NA)
-}
-
 ## Process extracted dataset ----
 data_processed <- data_extract %>%
   mutate(
@@ -94,12 +90,6 @@ data_processed <- data_extract %>%
     ),
 
     care_home_combined = care_home_tpp | care_home_code, # any carehome flag
-
-    # temporary until ehrql case statements no longer return character / factors
-    sev_obesity = boolfactor2bool(sev_obesity),
-    diabetes = boolfactor2bool(diabetes),
-    sev_mental = boolfactor2bool(sev_mental),
-    chronic_kidney_disease = boolfactor2bool(chronic_kidney_disease),
 
     immuno_any = immunosuppressed | asplenia | cancer | solid_organ_transplant |  hiv_aids,
 
