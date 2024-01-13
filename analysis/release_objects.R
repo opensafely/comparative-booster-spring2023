0fd2019feb9f240f1c627454b2ef77f049a86acf
wjchulme <25637345+wjchulme@users.noreply.github.com>
2024-01-13 08:13:24 +0000

    load sf

--- analysis/release_objects.R
+++ analysis/release_objects.R
@@ -6,6 +6,7 @@
 ## Import libraries ----
 library('tidyverse')
 library('here')
+library('fs')
 library('glue')
 
 
@@ -15,37 +16,37 @@ library('glue')
 ## post-matching ----
 
 for(cohort in c("age75plus", "cv")){
-  fs::dir_create(here("output", "release-objects", cohort, "prematch"))
+  dir_create(here("output", "release-objects", cohort, "prematch"))
 
-  fs::file_copy(here("output", cohort, "table1.csv"), fs::path(here("output", "release-objects", cohort, "prematch_table1.csv")), overwrite = TRUE)
-
+  file_copy(here("output", cohort, "table1.csv"), here("output", "release-objects", cohort, "prematch_table1.csv"), overwrite = TRUE)
+  file_copy(here("output", cohort, "total_rounded.csv"), here("output", "release-objects", cohort, "total_flowchart.csv"), overwrite = TRUE)
 
   for(matchset in c("A", "B")){
 
     output_dir <- here("output", "release-objects", cohort, matchset)
-    fs::dir_create(output_dir)
+    dir_create(output_dir)
 
     ## matching ----
 
-    fs::file_copy(here("output", cohort, matchset, "report", "table1.csv"), fs::path(output_dir, "match_table1.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "report", "data_coverage.csv"), fs::path(output_dir, "match_coverage.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "report", "flowchart.csv"), fs::path(output_dir, "match_flowchart.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "report", "table1.csv"), path(output_dir, "match_table1.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "report", "data_coverage.csv"), path(output_dir, "match_coverage.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "report", "flowchart_rounded.csv"), path(output_dir, "match_flowchart.csv"), overwrite = TRUE)
 
     ## Contrasts ----
 
-    fs::file_copy(here("output", cohort, matchset, "combined", "km_estimates_rounded.csv"), fs::path(output_dir, "km_estimates_rounded.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "combined", "contrasts_daily_rounded.csv"), fs::path(output_dir, "contrasts_daily_rounded.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "combined", "contrasts_cuts_rounded.csv"), fs::path(output_dir, "contrasts_cuts_rounded.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "combined", "contrasts_overall_rounded.csv"), fs::path(output_dir, "contrasts_overall_rounded.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "combined", "eventcounts.csv"), fs::path(output_dir, "eventcounts.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "combined", "followup_rounded.csv"), fs::path(output_dir, "followup_rounded.csv"), overwrite = TRUE)
-    fs::file_copy(here("output", cohort, matchset, "combined", "followup_treatment_rounded.csv"), fs::path(output_dir, "followup_treatment_rounded.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "combined", "km_estimates_rounded.csv"), path(output_dir, "km_estimates_rounded.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "combined", "contrasts_daily_rounded.csv"), path(output_dir, "contrasts_daily_rounded.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "combined", "contrasts_cuts_rounded.csv"), path(output_dir, "contrasts_cuts_rounded.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "combined", "contrasts_overall_rounded.csv"), path(output_dir, "contrasts_overall_rounded.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "combined", "eventcounts.csv"), path(output_dir, "eventcounts.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "combined", "followup_rounded.csv"), path(output_dir, "followup_rounded.csv"), overwrite = TRUE)
+    file_copy(here("output", cohort, matchset, "combined", "followup_treatment_rounded.csv"), path(output_dir, "followup_treatment_rounded.csv"), overwrite = TRUE)
 
   }
 }
 
 ## create text for output review issue ----
-fs::dir_ls(here("output", "release-objects"), type="file", recurse=TRUE) %>%
+dir_ls(here("output", "release-objects"), type="file", recurse=TRUE) %>%
   map_chr(~str_remove(., fixed(here()))) %>%
   map_chr(~paste0("- [ ] ", str_remove(.,fixed("/")))) %>%
   paste(collapse="\n") %>%
@@ -53,10 +54,11 @@ fs::dir_ls(here("output", "release-objects"), type="file", recurse=TRUE) %>%
 
 
 ## create command for releasing using osrelease ----
-fs::dir_ls(here("output", "release-objects"), type="file", recurse=TRUE) %>%
+dir_ls(here("output", "release-objects"), type="file", recurse=TRUE) %>%
   map_chr(~str_remove(., fixed(paste0(here(), "/")))) %>%
   #map_chr(~paste0("'",. ,"'")) %>%
   paste(., collapse=" ") %>%
   paste("osrelease", .) %>%
   writeLines(here("output", "osrelease-command.txt"))
+
 
